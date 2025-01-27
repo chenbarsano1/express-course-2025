766e7a5e4b178b6861113c8cc05d7e6d389e5d08
Chen Barsano <chenbarsano@gmail.com>
2025-01-27 17:00:21 +0200

    CRUD requests

--- server.js
+++ server.js
@@ -1,10 +1,13 @@
 import express from 'express'
 import path from 'path'
 import posts from './routes/posts.js'
-
 const port = process.env.PORT || 8000
 
 const app = express()
+
+// body parser middleware
+app.use(express.json())
+app.use(express.urlencoded({extended: false}))
 
 // setup static folder - declare one of our folders (public folder)
 // to be static - meaning we can just go to the url and it will work
@@ -12,7 +15,6 @@ const app = express()
 
 // Routes
 app.use('/api/posts', posts)
-
 
 
 app.listen(port, () => console.log(`Server is running on port ${port}`))
