f49e025d28789599a58b0d2db8db68b9b51d997f
Chen Barsano <chenbarsano@gmail.com>
2025-01-27 13:15:45 +0200

    request params

--- server.js
+++ server.js
@@ -1,11 +1,12 @@
 const express = require('express')
 const path = require('path')
+const port = process.env.PORT || 8000
 
 const app = express()
 
 // setup static folder - declare one of our folders (public folder)
 // to be static - meaning we can just go to the url and it will work
-app.use(express.static(path.join(__dirname, 'public')))
+// app.use(express.static(path.join(__dirname, 'public')))
 
 // app.get('/', (req, res) => {
 //     res.sendFile(path.join(__dirname, 'public', 'index.html'))
@@ -15,4 +16,21 @@ app.use(express.static(path.join(__dirname, 'public')))
 //     res.sendFile(path.join(__dirname, 'public', 'about.html'))
 // })
 
-app.listen(8000, () => console.log(`Server is running on port 8000`))
+let posts = [
+    {id: 1, title: 'Post 1'},
+    {id: 2, title: 'Post 2'},
+    {id: 3, title: 'Post 3'}
+]
+
+// get all posts
+app.get('/api/posts', (req, res) => {
+    res.json(posts)
+})
+
+// get single post
+app.get('/api/posts/:id', (req, res) => {
+    const id = parseInt(req.params.id)
+    res.json(posts.filter((post) => post.id === id))
+})
+
+app.listen(port, () => console.log(`Server is running on port ${port}`))
